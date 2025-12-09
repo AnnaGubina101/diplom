1ef79a2b37c17e1f4b56275700edc7573ff50196
Anna Gubina <anya.gubina101@gmail.com>
2025-12-08 20:01:58 -0500

    Доработка дипломной работы

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -1,11 +1,13 @@
 export default function Header() {
     const handleHeaderClick = () => {
-        window.location.href = "/"; 
+        window.location.reload(); 
     };
 
     return (
          <div className='header'>
-            <h1 className='header-main' onClick={handleHeaderClick}>Идем<span className='header__letter'>В</span>Кино </h1>
+            <h1 className='header-main' onClick={handleHeaderClick}>
+                Идем<span className='header__letter'>В</span>Кино
+            </h1>
         </div>
-    )
+    );
 }
